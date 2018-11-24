01022e55d031dde0dadeb3a3d223ef7af7a379aa
pjay79 <praveen.jayarajan@gmail.com>
2018-11-24 18:49:16 +1100

    Add setupTests.js

--- app/components/__tests__/Input.test.js
+++ app/components/__tests__/Input.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
-import 'jest-dom/extend-expect';
 import Input from '../Input';
 
 fireEvent.press = (node, init) => {
--- app/screens/__tests__/LoadingScreen.test.js
+++ app/screens/__tests__/LoadingScreen.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import 'jest-dom/extend-expect';
 import LoadingScreen from '../LoadingScreen';
 
 describe('LoadingScreen', () => {
